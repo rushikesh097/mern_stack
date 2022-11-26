8ba54b27545c4d05d316050bb883869da195e1df
Rushikesh Gaikwad <grushikesh907@gmail.com>
2022-11-26 17:53:50 +0530

    Routes added in Backend

--- server/index.js
+++ server/index.js
@@ -2,10 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const mysql = require("mysql");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
-const salt = "$2b$10$GHMgOdFTb5IcM5S5IW.1du";
 const userRouter = require("./routes/user");
 const collectionRouter = require("./routes/collection")
 
@@ -13,12 +10,6 @@ app.use(cors());
 
 app.use(express.json());
 
-const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "password",
-  database: "urbanmart",
-});
 
 app.use("/user",userRouter);
 
